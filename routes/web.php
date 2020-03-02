60549a991f0c9373e33f6094ce5f53da84904538
dumondmd <dmd.dumon@gmail.com>
2020-03-02 11:06:40 -0300

    Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,42 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
+    return "Produtos(s) {$idProduct}";
+});
+
+Route::get('/categoria/{flag}/posts', function($flag) {
+    return "Posts da categoria: {$flag}";
+});
+
+
+Route::get('/categorias/{flag}', function ($flag) {
+    return "Produtos da categoria {$flag}";
+});
+
+Route::match(['get', 'post'], '/match', function () {
+    return 'match';
+});
+
+Route::any('/any', function () {
+    return 'Any';
+});
+
+Route::post('/register', function () { 
+    return '';
+});
+
+
+Route::get('/empresa', function () {
+    return 'Sobre a empresa';
+});
+
+Route::get('/contato',function() {
+    return view('site.contact');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome');    
 });
+
+
