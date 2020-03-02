6406a25a099af39a10416603df417b6a051b09b6
dumondmd <dmd.dumon@gmail.com>
2020-03-02 16:04:25 -0300

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
+
+Route::resource('products', 'ProductController');
+/*
+Route::delete('products/{id}', 'ProductController@destroy')->name('product.destroy');
+Route::put('products/{id}', 'ProductController@update')->name('product.update');
+Route::get('products/{id}/edit', 'ProductController@edit')->name('product.edit');
+Route::get('products/create', 'ProductController@create')->name('products.create');
+Route::get('products/{id}', 'ProductController@show')->name('products.show');
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::post('products', 'ProductsController@store')->name('products.store');
+*/
+
+
 Route::get('/login', function () {
     return 'Login';
 })->name('login');
